4b28443c68f80610a9ae3948aabab3bc61eb187f
puopg <michaelchen101@gmail.com>
2015-04-01 19:23:55 -0700

    Adding index.html, homeController, and some dependencies. Moved bower_components into client.

---
+++ client/app/app.js
@@ -0,0 +1,15 @@
+(function(){
+
+  var app = angular.module('interview', ['ui.router']);
+
+  app.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+      $urlRouterProvider.otherwise('/');
+      $stateProvider
+        .state('home', {
+            url: '/',
+            controller: 'HomeCtrl',
+            templateUrl: 'app/home/home.html',
+        })
+  })
+  console.log('App loaded successfully');
+})();
---
+++ client/app/home/homeCtrl.js
@@ -0,0 +1,12 @@
+(function(){
+
+  angular
+    .module('interview')
+    .controller('HomeCtrl', HomeCtrl);
+
+  HomeCtrl.$inject = ['$scope'];
+
+  function HomeCtrl($scope){
+    
+  }
+})();
