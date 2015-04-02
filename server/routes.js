ca70309057704bd2802a7d61f7dc810f45a1e0e4
Garron Sanchez <garron.michael@gmail.com>
2015-04-02 12:21:26 -0700

    server static files

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,9 @@
-module.exports = function(app) {
+module.exports = function(app, express) {
+
+  // serve html and css 
+  app.use(express.static(__dirname + '../client'));
+  
+  // create routhes for authentication, and user and room data storage
   app.use('/auth', require('./auth');
   app.use('/user', require('./user');
   app.use('/room'), require('./room');
