997dc085c9fec5db4ea1691d311b3f9cb3401854
puopg <michaelchen101@gmail.com>
2015-04-03 15:45:58 -0700

    Modifying file names in server/db and added config to server

--- server/db/config.js
+++ server/db/config.js
@@ -3,11 +3,12 @@ var mongoose = require('mongoose');
 var mongoURI = 'mongodb://localhost/hackbox';
 var db = mongoose.connection;
 
-
 // CONNECTION: pending to database on local host; need notification if connected successfully or error occured
 mongoose.connect(mongoURI);
+
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback){
   console.log('MongoDB connection open!');
 });
+
