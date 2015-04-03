94eca2f5c9b998543fbdc728758c11243f8c97dd
Lauren McDonnell <mcdonnell88@gmail.com>
2015-04-03 12:31:32 -0700

    update mongo URI

--- server/db/config.js
+++ server/db/config.js
@@ -1,10 +1,12 @@
 // INITIATION: add mongoose to project and open connection to database on locally running MongoDB instance 
 var mongoose = require('mongoose');
-mongoose.connect(<MONGODB connection goes here>) --> example: 'mongodb://localhost/test'
+var mongoURI = 'mongodb://localhost/hackbox';
+var db = mongoose.connection;
 
 
 // CONNECTION: pending to database on local host; need notification if connected successfully or error occured
-var db = mongoose.connection;
+mongoose.connect(mongoURI);
+
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function(callback){
   console.log('MongoDB connection open!');
