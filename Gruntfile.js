8553a7890032a2f08682c7d7d9547676c30aa07a
Joshua Newman <jonewman1020@gmail.com>
2015-04-03 11:19:30 -0700

    Make env.config file, add it to gitignore and make gruntfile serve up

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,13 @@
 module.exports = function(grunt) {
- 
+  var localConfig;
+  try {
+    localConfig = require('env.config');
+  } catch(e) {
+    localConfig = {};
+  }
+
   grunt.initConfig({
+
     karma: {
       unit: {
         configFile: 'karma.conf.js',
@@ -19,13 +26,23 @@ module.exports = function(grunt) {
         files: ['client/app/**/*.js', 'Spec/unit/**/*.js'],
         tasks: ['karma:unit:run']
       }
-    }
+    },
+    env: {
+      test: {
+        NODE_ENV: 'test'
+      },
+      prod: {
+        NODE_ENV: 'production'
+      },
+      all: localConfig
+    },
     });
 
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-karma');
-  grunt.registerTask('devmode', ['karma:unit', 'watch']);
-  grunt.registerTask('test', ['karma:travis']);
+  grunt.registerTask('devmode', ['env:all', 'env:test', 'karma:unit', 'watch']); //development build, watch for changes
+  grunt.registerTask('test', ['env:all', 'env:test', 'karma:travis']);  //test build
+  // grunt.registerTask('serve', ['build', 'env:all','env:prod']); //production build
 
 };
 
