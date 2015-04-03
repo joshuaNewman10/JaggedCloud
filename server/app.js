4b1746931b6cd859f05b915578e2663b393dbd73
Lauren McDonnell <mcdonnell88@gmail.com>
2015-04-03 11:22:54 -0700

    add database config and connetion to add.js

--- server/app.js
+++ server/app.js
@@ -8,3 +8,18 @@ var port = process.env.port || 3000;
 require('./routes')(app, express);
 
 app.listen(port);
+
+
+
+
+// INITIATION: include mongoose and open connection to database on locally running MongoDB instance
+var mongoose = require('mongoose');
+mongoose.connect(<MONGODB connection goes here>) --> example: 'mongodb://localhost/test'
+
+
+// CONNECTION: pending to database on local host; need notification if connected successfully or error occured
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback){
+  console.log('connection is open!');
+});
