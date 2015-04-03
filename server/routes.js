79305ab84c7292b40b879b390eca5d3d145903a5
Garron Sanchez <garron.michael@gmail.com>
2015-04-02 19:48:18 -0700

    pass in routers

--- server/routes.js
+++ server/routes.js
@@ -1,10 +1,25 @@
+var morgan = require('morgan');
+
 module.exports = function(app, express) {
 
+  // create routers
+  var authRouter = express.Router();
+  var userRouter = express.Router();
+  var roomRouter = express.Router();
+
+  // http request logger for development 
+  app.use(morgan('dev'));
+
   // serve html and css 
-  app.use(express.static(__dirname + '../client'));
+  app.use(express.static(__dirname + '/../client'));
   
-  // create routhes for authentication, and user and room data storage
-  app.use('/auth', require('./auth');
-  app.use('/user', require('./user');
-  app.use('/room'), require('./room');
+  // create paths for authentication, and user and room data storage
+  app.use('/auth', authRouter);
+  app.use('/user', userRouter);
+  app.use('/room', roomRouter);
+  
+  // require the needed route files and pass in the router
+  require('./auth/authRoutes.js')(authRouter);
+  require('./users/userRoutes.js')(userRouter);
+  require('./rooms/roomRoutes.js')(roomRouter);
 };
