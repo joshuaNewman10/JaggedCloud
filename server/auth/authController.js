b8602aa43b5068c44519ddceae2efcdd07d3ef70
Garron Sanchez <garron.michael@gmail.com>
2015-04-04 15:18:39 -0700

    refactor logic out of routes and into controller

--- server/auth/authController.js
+++ server/auth/authController.js
@@ -1,3 +1,25 @@
+var passport = require('./githubConfig.js').passport;
+
 module.exports.signin = function(req, res){
-  console.log("Trying to sign in.");
+    passport.authenticate('github'),
+    function(req, res){
+      // The request will be redirected to Github for authentication, so this
+      // function will not be called.
+  }
+};
+
+module.exports.callback = function(req, res) {
+    passport.authenticate('github', { successRedirect: '/', failureRedirect: '/signin' });
+};
+
+module.exports.logout = function(req, res) {
+  req.session.destroy(function(err){
+    if (err) console.error('Error destroying session: ' + err);
+    req.logout();
+    res.redirect('/');
+  });
+}
+
+module.exports.isAuthenticated = function(req, res) {
+  res.send(req.isAuthenticated() ? req.user : false);
 }
--- server/auth/authRoutes.js
+++ server/auth/authRoutes.js
@@ -1,31 +1,12 @@
 var authController = require('./authController.js');
-var passport = require('./githubConfig.js').passport;
 
 module.exports = function(router){
 
-  router.get('/github',
-    passport.authenticate('github'),
-    function(req, res){
-      // The request will be redirected to Github for authentication, so this
-      // function will not be called.
-  });
+  router.get('/github', authController.signin);
 
-  router.get('/github/callback', 
-    passport.authenticate('github', { failureRedirect: '/' }),
-    function(req, res) {
-      // Successful authentication, redirect home.
-      res.redirect('/');
-  });
+  router.get('/github/callback', authController.callback);
 
-  router.post('/checkloggedin', function(req, res){
-    res.send(req.isAuthenticated() ? req.user : false);
-  });
+  router.post('/checkloggedin', authController.isAuthenticated);
 
-  router.get('/logout', function(req, res){
-    req.session.destroy(function(err){
-      if (err) console.error('Error destroying session: ' + err);
-      req.logout();
-      res.redirect('/');
-    });
-  });
+  router.get('/logout', authController.logout);
 };
