13f4b989f66a5b86e42cf6d4ce856d9bb16a10f8
Garron Sanchez <garron.michael@gmail.com>
2015-04-04 17:07:15 -0700

    add User.findOrCreate

--- server/auth/githubConfig.js
+++ server/auth/githubConfig.js
@@ -14,13 +14,13 @@ var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
 //   have a database of user records, the complete GitHub profile is serialized
 //   and deserialized.
 passport.serializeUser(function(user, done) {
-  done(null, user.id);
+  console.log('Serializing: ', user)
+  done(null, user);
 });
 
-passport.deserializeUser(function(id, done) {
-  User.findById(id, function(id, done){
-    done(err, user);
-  });
+passport.deserializeUser(function(obj, done) {
+  console.log('Deserializing: ', obj);
+  done(null, user);
 });
 
 // Use the GitHubStrategy within Passport.
@@ -30,19 +30,27 @@ passport.deserializeUser(function(id, done) {
 passport.use(new GitHubStrategy({
     clientID: GITHUB_CLIENT_ID,
     clientSecret: GITHUB_CLIENT_SECRET,
-    callbackURL: process.env.domain + 'auth/github/callback'
+    callbackURL: 'http://localhost:3000/auth/github/callback'
   },
   function(accessToken, refreshToken, profile, done) {
+    // my code: 
+    User.findOrCreate({ githubId: profile.id }, function (err, user) {
+          console.log(user);
+          return done(err, user);
+        });
+
     // asynchronous verification, for effect...
-    process.nextTick(function () {
+    // process.nextTick(function () {
+
+
       
       // To keep the example simple, the user's GitHub profile is returned to
       // represent the logged-in user.  In a typical application, you would want
       // to associate the GitHub account with a user record in your database,
       // and return that user instead.
-      return done(null, profile);
-    });
+      // return done(null, profile);
+    // });
   } 
 ));
 
-module.exports.passport = passport;
+module.exports = passport;
