d41ec883b312502c9131e809082d4fdcad2c0da2
Joshua Newman <jonewman1020@gmail.com>
2015-04-07 20:06:23 -0700

    Refactor socketFactory comment style

--- client/app/sockets/socketFactory.js
+++ client/app/sockets/socketFactory.js
@@ -1,8 +1,9 @@
-//socketFactory.js
-//This is a service designed to sync the canvas and text editor for both 
-//members of a room. 
-//It works using sockets.io where this factory wraps the socket Object
-
+/**socketFactory.js
+ *
+ * This is a service designed to sync the canvas and text editor for both 
+ * members of a room. 
+ * It works using sockets.io where this factory wraps the socket Object
+ */
 (function() {
   angular
   .module('hackbox')
@@ -20,8 +21,10 @@
 
   //// IMPLEMENTATION ////
 
-    //Function: Sockets.on()
-    //This Function listens for generic socket events from the server
+    /**Function: Sockets.on()
+     *
+     * This Function listens for generic socket events from the server
+     */
     function on(eventName, callback) {
       socket.on(eventName, function() {
         var args = Array.prototype.slice.call(arguments);
@@ -31,9 +34,11 @@
       });
     }
     
-    //Function: Sockets.emit()
-    //This Function emits generic socket events, sends the data to the server
-    //and then also will execute a callback if specified
+    /**Function: Sockets.emit()
+     *
+     *This Function emits generic socket events, sends the data to the server
+     *and then also will execute a callback if specified
+     */
     function emit(eventName, data, callback) {
       socket.emit(eventName, data, function() {
         var args = Array.prototype.slice.call(arguments);
