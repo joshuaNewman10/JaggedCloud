395fb805ba18900cf201f63f617d80a0742c005f
puopg <michaelchen101@gmail.com>
2015-04-07 19:50:44 -0700

    Refactoring text editor and video into seperate controllers

--- client/app/textEditor/textEditorCtrl.js
+++ client/app/textEditor/textEditorCtrl.js
@@ -1,23 +1,18 @@
 /** 
- * roomCtrl.js
+ * textEditorCtrl.js
  * 
- * This is the controller responsible for the room view. 
- * Anything needing to change the room view should be placed here. 
+ * This is the controller responsible for the text editors in a room.
  */
 
 (function(){
 
   angular
     .module('hackbox')
-    .controller('roomCtrl', RoomCtrl);
+    .controller('textEditorCtrl', TextEditorCtrl);
 
-  RoomCtrl.$inject = ['$scope', '$sce', '$http', 'Video', 'Drawing', 'TextEditor'];
+  TextEditorCtrl.$inject = ['$scope' ,'TextEditor'];
 
-  function RoomCtrl($scope, $sce, $http, Video, Drawing, TextEditor){
-    $scope.userVideoSource = null;
-    $scope.peerVideoSource = null;
-    $scope.drawingCanvas = null;
-    $scope.showCanvas = false;
+  function TextEditorCtrl($scope, TextEditor){
     $scope.editors = [];
     var MAX_EDITORS = 5;
 
@@ -27,121 +22,27 @@
     });
 
     /**  
-     * Function: RoomCtrl.init()
+     * Function: TextEditorCtrl.init()
      * This function will initialize all entities upon switching the the room state.
      */
     $scope.init = function(){
-      $scope.initializeVideo('hackbox');
-      $scope.initializeCanvas('canvas-container');
       $scope.addTextEditor();
     };
 
     /**
-     * Function: RoomCtrl.uninit()
+     * Function: TextEditorCtrl.uninit()
      * This function will be called when we leave a room.
-     * It will cleanup all entities within a given room such as video and text editors. 
+     * It will destroy all editors currently in use. 
      */
     $scope.uninit = function(){
-      console.log('Leaving Room, shutting down video, canvas and removing listeners.');
-      // Remove Video/Audio
-      Video.getIcecommInstance().leave(true);
-
-      // Remove Canvas
-      Drawing.removeCanvas('canvas-container');
-
+      console.log('Destroying text editors');
       // Remove all text editors
       $scope.editors.forEach(function(editor){
         editor.editor.destroy();
       });
     };
-
     /**
-     * Function: RoomCtrl.initializeVideo(roomName)
-     * This function will initialize the Video component of the room.
-     *
-     * @param roomName: A string representing the roomname to join. 
-     */
-    $scope.initializeVideo = function(roomName){
-      // Create the Icecomm object and get the instance of it.
-      var comm = Video.getIcecommInstance();
-
-      // Connect to the correct room. Room supports a maximum of 2 people. 
-      comm.connect(roomName, {limit: 2, audio: false});
-
-      // Register user video connected event
-      comm.on('local', function(peer) {
-        $scope.userVideoSource = $sce.trustAsResourceUrl(peer.stream);
-        $scope.$digest();
-      });
-
-      // Register peer connect/disconnect event
-      comm.on('connected', function(peer) {
-          $scope.peerVideoSource = $sce.trustAsResourceUrl(peer.stream);
-          $scope.$digest();
-      });
-
-      comm.on('disconnect', function(peer) {
-          $scope.peerVideoSource = '';
-      });
-    };
-
-    /**
-     * Function: RoomCtrl.initializeCanvas(containerClassName)
-     * This function will append a canvas element to the room. 
-     *
-     * @param containerClassName: NOT_USED_CURRENTLY
-     */
-    $scope.initializeCanvas = function(containerClassName) {
-      //create a new canvas object and get its reference
-      var canvas = Drawing.makeCanvas();
-      $('.canvas-container').append(canvas);
-
-      var canvasFabric = new fabric.Canvas('drawingCanvas', {
-        isDrawingMode: true
-      });
-
-      canvasFabric.setHeight(400);
-      canvasFabric.setWidth(650);
-
-      //Give roomcontroller a reference to the canvas
-      $scope.drawingCanvas = canvasFabric;
-    };
-
-    /**
-     * Function: RoomCtrl.saveData()
-     * This function is called when the user clicks the saveCanvas button
-     * It converts the canvas data into a png image string and then
-     * makes a request to our server to store the image in the database
-     */
-    $scope.saveData = function() {
-      var drawingData = {
-        username: 'testname123',
-        data: $scope.drawingCanvas.toDataURL()
-      };
-
-      var request = {
-        method: 'POST',
-        url: '/room/save',
-        headers: {
-         'Content-Type': 'json'
-        },
-        data: { 
-          roomId: '1',
-          canvas: JSON.stringify(drawingData)
-        }
-      };
-
-      $http(request)
-      .success(function(response){
-        console.log('http response', response);
-      })
-      .error(function(error){
-        console.log('error', error);
-      });
-    };
-    
-    /**
-     * Function: RoomCtrl.addTextEditor()
+     * Function: TextEditorCtrl.addTextEditor()
      * This function will add a new text editor to the DOM. 
      */
     $scope.addTextEditor = function(){
@@ -163,7 +64,7 @@
     };
 
     /**
-     * Function: RoomCtrl.removeTextEditor(editorId)
+     * Function: TextEditorCtrl.removeTextEditor(editorId)
      * This function will remove a text editor from the DOM.
      *
      * @param editorId: An integer representing the ID of an editor object. Range(0 - MAX_EDITORS)
@@ -196,7 +97,7 @@
     };
 
     /**
-     * Function: RoomCtrl.removeTextEditor(editorId)
+     * Function: TextEditorCtrl.removeTextEditor(editorId)
      * This function will set the editor in the collection with the matching Id as the active editor 
      *
      * @param editorId: An integer representing the ID of an editor object. Range(0 - MAX_EDITORS)
@@ -213,14 +114,6 @@
       // Focus on the editor and set cursor to end.
       $scope.editors[editorToFocusOn].editor.focus();
       $scope.editors[editorToFocusOn].editor.navigateLineEnd();
-    };
-
-    /**
-     * Function: RoomCtrl.toggleCanvas()
-     * This function will toggle the canvas on/off.
-     */
-    $scope.toggleCanvas = function(){
-      $scope.showCanvas = !$scope.showCanvas;
     };
 
     /**
@@ -274,7 +167,7 @@
                           }).indexOf(editorId);
       return idx;
     }
-    // Call the initialize function
     $scope.init();
   }
+
 })();
