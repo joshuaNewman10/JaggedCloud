e8e703bb119d73af4d9d7f7847def7da0070e4c3
Lauren McDonnell <mcdonnell88@gmail.com>
2015-04-10 16:44:03 -0700

    merge conflicts

--- server/users/userController.js
+++ server/users/userController.js
@@ -3,15 +3,24 @@ module.exports.save = function(req, res) {
 };
 
 
+// below function can be replace current auth query
 
 
-// INSTANTIATION: example
-// var garron = new User({
-//   name: {
-//     first: 'Garron',
-//     last: 'Sanchez'
-//   },
-//   email: ImAlwaysHungry@food.com,
-//   profile_photo: <some string for his photo>,
-//   rooms: [] --> this is initially empty I guess
-// });
+// Check if user is already in the database -- if not, create a new user
+// TODO: use .then() method to save the newly created user to the database
+var findOrCreateUser = function(githubID) {
+
+  var query = User.findOne({'githubId': githubID});
+  
+  if(!query) {
+    User.create({'githubId': githubId}, function(err, user) {
+      if (err) { console.log(err); }
+      else { console.log('githubId is ' + user.githubId); }
+    });
+  }
+
+}
+
+
+
+// NOTE: the method .create() creates a new document AND saves it to the database -- do not have to also execute .save(). In fact, that caused an error for that we couldn't figure out at first
