2f3da4821733f15267488527b9fcb6e8f02f5bec
puopg <michaelchen101@gmail.com>
2015-04-10 11:49:09 -0700

    Testing home room display

--- client/app/home/homeCtrl.js
+++ client/app/home/homeCtrl.js
@@ -13,6 +13,9 @@
   HomeCtrl.$inject = ['$scope' ,'$modal', '$log', 'Auth'];
 
   function HomeCtrl($scope, $modal, $log, Auth){
+
+    $scope.upcomingInterviews = [];
+
     /**
      * Function: HomeCtrl.logout()
      * This function will log the user out.
@@ -22,7 +25,48 @@
       console.log('Logging out!');
     };
 
+    $scope.joinRoom = function(interview){
+      console.log('Joining: ' + interview.roomId)
+    } 
 
+    $scope.testingInit = function(){
+      var room1 = {
+        company: 'Intel',
+        start_time: '10:30',
+        created_by: 'Kwong Chan',
+        roomId: "JA234N2Kr2r2f2J6NK34HAasdfasdfJ3L41K4"
+      };
+      $scope.upcomingInterviews.push(room1);
+
+      var room2 = {
+        company: 'Google',
+        start_time: '12:30',
+        created_by: 'Michael Chen',
+        roomId: "JFLAK20f93rnfasd-11ajksdfj"
+
+      };
+      $scope.upcomingInterviews.push(room2);
+
+      var room3 = {
+        company: 'AirBnB',
+        start_time: '2:14',
+        created_by: 'Richard Kho',
+        roomId: "37SPFj320v20fgn0=f2-0fj"
+
+      };
+      $scope.upcomingInterviews.push(room3);
+
+      var room4 = {
+        company: 'Hack Reactor',
+        start_time: '9:25',
+        created_by: 'Marcus Phillips',
+        roomId: "kjalhsdf9920ndASDLKJA(@2od0f2"
+
+      };
+      $scope.upcomingInterviews.push(room4);
+    };
+
+    $scope.testingInit();
     // Upon loading home, if the user is authenticated
     // Get his list of interviews upcoming. 
     // Populate the list using an ng-repeat and make each div clickable
