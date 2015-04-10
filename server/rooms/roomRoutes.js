2658046b47e7daf4441861ce8979f749e45ff210
Lauren McDonnell <mcdonnell88@gmail.com>
2015-04-09 20:38:20 -0700

    update schema xml

--- server/rooms/roomRoutes.js
+++ server/rooms/roomRoutes.js
@@ -1,9 +1,6 @@
 var roomController = require('./roomController.js');
 
 
-// TODO: specify /url for the following
-
-
 module.exports = function(router){
 
   router.post('/save', roomController.save);
