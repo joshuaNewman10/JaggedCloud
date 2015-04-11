92b1da1290a118bd6026682aa413ccf3cc704bd4
Lauren McDonnell <mcdonnell88@gmail.com>
2015-04-11 09:24:07 -0700

    resolve merge conflicts

--- server/db/models/roomModel.js
+++ server/db/models/roomModel.js
@@ -5,21 +5,21 @@ var mongoose = require('mongoose');
 // SCHEMAS: each schema maps to a 'collection' in MongoDB (analogous to SQL table) and defines the shape of the 'documents' within that colletion (documents are analogous to a row in a SQL table)
 var roomSchema = new mongoose.Schema({
 
+  is_open: {
+    type: Boolean,
+    default: false,
+  },
+
   date_created: {
     type: Date,
     default: Date.now
   },
 
-  is_open: {
-    type: Boolean,
-    default: false,
-  },
-  
-  canvas: String,
-  text: [String],
-  notes: String,
   start_time: Date,
   created_by: String, // githubId
+  canvas: String,
+  text: [String],
+  notes: String
 });
 
 
--- server/rooms/roomController.js
+++ server/rooms/roomController.js
@@ -1,32 +1,32 @@
-var Room = require('../db/models/roomModel');
-var User = require('../db/models/userModel');
+// var Room = require('../db/models/roomModel');
+// var User = require('../db/models/userModel');
 
-var handleError = function(error) {
-  console.log('the following error has occurred: ' + error);
-};
+// var handleError = function(error) {
+//   console.log('the following error has occurred: ' + error);
+// };
 
 
-// TODO: set created_by to githubId -- need to receive github id
+// // TODO: set created_by to githubId -- need to receive github id
 
-module.exports.create = function(req, res) {
-  console.log(req.body);
-  var user = req.body.name;
-  var startTime = req.body.time;
-  var email = req.body.email;
-  var name = req.body.name;
-  var githubId = req.body.githubId;
-  var startTime = req.body.time;
-  var isOpen = Date.now() >= Date.parse(startTime);
+// module.exports.create = function(req, res) {
+//   console.log(req.body);
+//   var user = req.body.name;
+//   var startTime = req.body.time;
+//   var email = req.body.email;
+//   var name = req.body.name;
+//   var githubId = req.body.githubId;
+//   var startTime = req.body.time;
+//   var isOpen = Date.now() >= Date.parse(startTime);
 
-  Room.create({ created_by: email, start_time: startTime, is_open: isOpen }, function(err, room){
-    if (err) { handleError(err); }
-    else if (room) {
-      console.log('room successfully created! ' + room);
-      res.send(201, room);
-    }      
-  });
+//   Room.create({ created_by: email, start_time: startTime, is_open: isOpen }, function(err, room){
+//     if (err) { handleError(err); }
+//     else if (room) {
+//       console.log('room successfully created! ' + room);
+//       res.send(201, room);
+//     }      
+//   });
 
-};
+// };
 
 // update pattern: Model.update(conditions, doc, [options], [callback])
 // module.exports.save = function(req, res) {
