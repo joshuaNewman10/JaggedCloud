c2990f8465447c3ee409da9a9d52e73378c64811
Garron Sanchez <garron.michael@gmail.com>
2015-04-11 17:55:32 -0700

    fix conflicts

---
+++ jaggedCloudNotes.js
@@ -0,0 +1,46 @@
+// JAGGED CLOUD
+
+/*
+
+Did not use GitHub Issues, what is Waffle.io?
+Advice: Set up slack, notetaker, trello, spike, meetings: after sprint reflection, after lunch, after dinner and before we leave
+
+Strengths: Angular, Backbone, SQL, Azure
+Fair: Node, Express, HTML/CSS
+Weaknesses: Mongo, Grunt, Gulp, Mocha/Chai, D3, **authentication**
+Tech Goals: node/express server, angularui-router, mongoDB schema, heroku or AWS, gunt continuous integration, facebook & twitter authentication, testing
+Group Goals: want everyone to feel like their contribution is significant and that everyone understands the whole project
+
+Josh
+  S: BackendDB manipulation, system architecture/design, bash and regex
+  W: backbone, angular, to grind and not speak up
+  G: Data-science, testing, deployment, good day-to-day routines, wants to work with a solid dataset manipulation
+   : angular, deployment, testing
+
+Lauren
+  S: whiteboarding, drawing out thoughts, verbalizing thoughts, schema design
+  W: understanding the moving parts and how all the files are communicating to each other, really likes modularity, spatial learning, backend servers
+  G: better understand complexity of an app, esp servers and how all the files talk to each other, really wants to know what every person did, step by
+     step decisions
+   : server, deployment, database, deployment authentication
+
+Michael 
+  S: backend, handle large code base, understanding different parts and ignoring complexity, learn APIs quickly
+  W: CSS/HTML, jQuery, user experience/interface
+  G: angular, not servers
+
+Ideas:
+
+  fitness app,
+  SOS distress,
+  Wayfare,
+  SnackPlant,
+  Hype Machine for news 
+  i hate being crowded on the train,
+  i hate waking up in the morning
+  i love the ocean
+  knowing up my family is doing
+  political news app maybe 
+  crime notifications while abroad
+  Tinder for friends
+
