75e3ae403c1f9ce8384ea3902730edfe455bea39
Garron Sanchez <garron.michael@gmail.com>
2015-04-11 17:16:22 -0700

    comment config process and clean up old code

--- server/auth/githubConfig.js
+++ server/auth/githubConfig.js
@@ -3,16 +3,12 @@ var passport = require('passport');
 var GitHubStrategy = require('passport-github').Strategy;
 var User = require('../db/models/userModel.js');
 
-var GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
-var GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete GitHub profile is serialized
-//   and deserialized.
+/* Passport session setup.
+*   To support persistent login sessions, Passport needs to be able to
+*   serialize users into and deserialize users out of the session. Here
+*   we store the user's github id when serializing, and find the user by 
+*   github id when deserializing.
+*/
 passport.serializeUser(function(user, done) {
   console.log('Serializing: ', user.github_id);
   done(null, user.github_id);
@@ -23,17 +19,18 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-// Use the GitHubStrategy within Passport.
-//   Strategies in Passport require a `verify` function, which accept
-//   credentials (in this case, an accessToken, refreshToken, and GitHub
-//   profile), and invoke a callback with a user object.
+/* Use the GitHubStrategy within Passport.
+*   Strategies in Passport require a `verify` function, which accept
+*   credentials (in this case, an accessToken, refreshToken, and GitHub
+*   profile), and invoke a callback with a user object.
+*/
 passport.use(new GitHubStrategy({
-    clientID: GITHUB_CLIENT_ID,
-    clientSecret: GITHUB_CLIENT_SECRET,
+    // github client id and secret should not be stored on your repo and should exist as environmental variables
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
     callbackURL: process.env.GITHUB_CB || 'http://localhost:3000/auth/github/callback'
   },
   function(accessToken, refreshToken, profile, done) {
-    // my code: 
     User.findOne({ github_id: profile.id }, function(err, user) {
       if(err) {
         console.error('Error: ', err);
@@ -57,18 +54,6 @@ passport.use(new GitHubStrategy({
         return done(null, user);
       }
     });
-
-    // asynchronous verification, for effect...
-    // process.nextTick(function () {
-
-
-      
-      // To keep the example simple, the user's GitHub profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the GitHub account with a user record in your database,
-      // and return that user instead.
-      // return done(null, profile);
-    // });
   } 
 ));
 
