93189d74d96e9dd5651e7050dcdf4b78f2da2f81
Garron Sanchez <garron.michael@gmail.com>
2015-04-14 12:16:02 -0700

    delete duplicate code

--- server/rooms/roomRoutes.js
+++ server/rooms/roomRoutes.js
@@ -6,8 +6,6 @@ module.exports = function(router){
 
   router.post('/create', roomController.create);
 
-  router.get('/get', roomController.fetchOne);
-
   router.get('/get:id', roomController.fetchOne);
 
   router.get('/home', roomController.fetchAll);
