8e25601138f20cc207afca3294b36002f8448e78
Joshua Newman <jonewman1020@gmail.com>
2015-04-13 18:46:01 -0700

    Change travis to run Chrome, Firefox

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,7 +27,7 @@ module.exports = function(grunt) {
       travis: {
         configFile: 'karma.conf.js',
         singleRun: true,
-        browsers: ['PhantomJS']
+        browsers: ['Chrome', 'Firefox']
       }
     },
     nodemon: {
