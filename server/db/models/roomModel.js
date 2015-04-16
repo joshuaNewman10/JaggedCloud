37b311e1ce7e39272d23a6f69139aa4e73733952
Lauren McDonnell <mcdonnell88@gmail.com>
2015-04-15 19:46:29 -0700

    interview room state notes

--- server/db/models/roomModel.js
+++ server/db/models/roomModel.js
@@ -5,14 +5,25 @@ var mongoose = require('mongoose');
 // SCHEMAS: each schema maps to a 'collection' in MongoDB (analogous to SQL table) and defines the shape of the 'documents' within that colletion (documents are analogous to a row in a SQL table)
 var roomSchema = new mongoose.Schema({
 
+// database
+  // remove is_open and is_completed -- no booleans at all
+  // add end time -- defaults to start time +24 hours (for now at least...)
+
+// ALSO: function that takes in the start and end time, then returns the 'current state'
+  // current state could be: (1) INCOMPLETE (not complete / not active) (2) LIVE (not complete / active) (3) COMPLETE (complete / not active) 
+
+// front-end
+  // add 'close' button, which sets the end time to Date.now()
+  // also 'open' button, which sets the start time to Date.now()
+
   is_open: {
     type: Boolean,
     default: false,
   },
 
-  is_writable: {
+  is_completed: {
     type: Boolean,
-    default: true
+    default: false
   },
 
   date_created: {
