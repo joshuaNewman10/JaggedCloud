fbb0731125865ae463cfd2507213131a42a84ada
Garron Sanchez <garron.michael@gmail.com>
2015-04-16 19:07:23 -0700

    create 404Ctrl and template

---
+++ client/app/404/404Ctrl.js
@@ -0,0 +1,53 @@
+/**
+ * pastCtrl.js
+ *
+ * This is the controller responsible for the past interviews page. 
+ */ 
+
+(function(){
+
+  angular
+    .module('hackbox')
+    .controller('404Ctrl', bigcontroller);
+
+  bigcontroller.$inject = ['$scope' ,'$modal', '$state','$log', 'Auth', 'Room'];
+
+  function bigcontroller($scope, $modal, $state, $log, Auth, Room){
+    $scope.showCreateInterview = false;
+    $scope.showLoadingCreateInterview = false;
+    $scope.showLogout = false;
+    $scope.incompleteInterviews = [];
+    $scope.newInterview = {};
+
+    /**
+     * Function: 404Cntrl.init()
+     * This function will initialize the home page. If the user is logged in,
+     * they will see a list of interviews coming up. Otherwise nothing. 
+     */
+    $scope.init = function(){
+      // Check if the user is logged in
+      Auth.isAuthenticated().then(function(response){
+        if(response.data){
+          console.log("User is logged in, display logout.")
+          $scope.showLogout = true;
+        }
+        else{
+          console.log('User is not logged in');
+        }
+      });
+    };
+
+    /**
+     * Function: 404Ctrl.logout()
+     * This function will log the user out.
+     */
+    $scope.logout = function () {
+      Auth.logout().then(function(){
+        $scope.showLogout = false;
+      });
+      console.log('Logging out!');
+    };
+
+    $scope.init();
+  }
+})();
