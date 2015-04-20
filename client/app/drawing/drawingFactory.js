0d517e22d20ac09cfd1c364c809d2553e4d9697c
Joshua Newman <jonewman1020@gmail.com>
2015-04-20 15:46:42 -0700

    Add message to dispaly when another user is drawing

--- client/app/drawing/drawingFactory.js
+++ client/app/drawing/drawingFactory.js
@@ -76,8 +76,6 @@
        Sockets.emit('join room', {roomName: $stateParams.roomId});
       });
 
-      Sockets.on('toggleDrawingMessage', toggleDrawingMessage);
-
       Sockets.on('coordinates', updateCanvas);
 
       Sockets.on('clearCanvas', function() {
@@ -112,10 +110,6 @@
       _fabricCanvas.freeDrawingBrush.color = '#FFFFFF';      
       _currentlyErasing = !_currentlyErasing;
      }
-    }
-
-    function toggleDrawingMessage() {
-      console.log("STOP DRAWING YOU FOOL!"); 
     }
 
     function stopIO() {
